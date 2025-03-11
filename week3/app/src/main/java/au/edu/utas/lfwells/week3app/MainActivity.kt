5e717950859653ed40f7aa2a6464ca08a8a8ed17
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-03-11 14:00:36 +1100

    live coding stuff from the lecture

--- week3/app/src/main/java/au/edu/utas/lfwells/week3app/MainActivity.kt
+++ week3/app/src/main/java/au/edu/utas/lfwells/week3app/MainActivity.kt
@@ -1,16 +1,29 @@
 package au.edu.utas.lfwells.week3app
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         setContentView(R.layout.activity_main)
+
+        val myClickableButton = findViewById<Button>(R.id.myButton)
+        myClickableButton.setOnClickListener {
+            myClickableButton.text = "Clicked!"
+            Log.d("MY_TAG", "Button clicked!")
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
