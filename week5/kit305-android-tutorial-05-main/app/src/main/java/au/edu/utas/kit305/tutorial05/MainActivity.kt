458ffa2ec92fc69c42bf9ba19f561ed5e74a61b0
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-04-01 13:54:32 +1100

    added swipe to delete

--- week5/kit305-android-tutorial-05-main/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
+++ week5/kit305-android-tutorial-05-main/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
@@ -1,16 +1,27 @@
 package au.edu.utas.kit305.tutorial05
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
+import android.widget.SimpleAdapter
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import au.edu.utas.kit305.tutorial05.databinding.ActivityMainBinding
 import au.edu.utas.kit305.tutorial05.databinding.ActivityMovieDetailsBinding
 import au.edu.utas.kit305.tutorial05.databinding.MyListItemBinding
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.toObject
 import com.google.firebase.ktx.Firebase
@@ -23,6 +34,8 @@ const val MOVIE_INDEX = "Movie_Index"
 class MainActivity : AppCompatActivity()
 {
     private lateinit var ui : ActivityMainBinding
+
+    private lateinit var moviesCollectionRef : CollectionReference
 
     override fun onResume() {
         super.onResume()
@@ -42,6 +55,16 @@ class MainActivity : AppCompatActivity()
         //vertical list
         ui.myList.layoutManager = LinearLayoutManager(this)
 
+        //set up swipe to delete
+        val swipeHandler = object : SwipeToDeleteCallback(this) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val adapter = ui.myList.adapter as MovieAdapter
+                adapter.removeAt(viewHolder.adapterPosition)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
+        itemTouchHelper.attachToRecyclerView(ui.myList)
+
         //get db connection
         val db = Firebase.firestore
         Log.d("FIREBASE", "Firebase connected: ${db.app.name}")
@@ -53,7 +76,7 @@ class MainActivity : AppCompatActivity()
             duration = 9001F
         )
 
-        val moviesCollectionRef = db.collection("movies")
+        moviesCollectionRef = db.collection("movies")
 
         /*
         moviesCollectionRef
@@ -148,6 +171,99 @@ class MainActivity : AppCompatActivity()
                 startActivity(i)
             }
         }
+
+        fun removeAt(position: Int) {
+
+            //delete from the databaes
+            moviesCollectionRef.document(movies[position].id!!)
+                .delete()
+                .addOnSuccessListener {
+                    Log.d(FIREBASE_TAG, "DocumentSnapshot successfully deleted!")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(FIREBASE_TAG, "Error deleting document", e)
+                }
+
+            items.removeAt(position)
+            notifyItemRemoved(position)
+
+            ui.lblMovieCount.text = "Movie Count: ${items.size}"
+        }
     }
 }
+//https://github.com/kitek/android-rv-swipe-delete/blob/master/app/src/main/java/pl/kitek/rvswipetodelete/SwipeToDeleteCallback.kt
+abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
+    private val deleteIcon = ContextCompat.getDrawable(context, android.R.drawable.ic_delete)
+    private val intrinsicWidth = deleteIcon?.intrinsicWidth
+    private val intrinsicHeight = deleteIcon?.intrinsicHeight
+    private val background = ColorDrawable()
+    private val backgroundColor = Color.parseColor("#f44336")
+    private val clearPaint = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
+
+
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        val itemView = viewHolder.itemView
+        val itemHeight = itemView.bottom - itemView.top
+        val isCanceled = dX == 0f && !isCurrentlyActive
+
+        if (isCanceled) {
+            clearCanvas(c, itemView.right + dX, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat())
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            return
+        }
+
+        // Draw the red delete background
+        background.color = backgroundColor
+        background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+        background.draw(c)
+
+        // Calculate position of delete icon
+        val deleteIconTop = itemView.top + (itemHeight - intrinsicHeight!!) / 2
+        val deleteIconMargin = (itemHeight - intrinsicHeight!!) / 2
+        val deleteIconLeft = itemView.right - deleteIconMargin - intrinsicWidth!!
+        val deleteIconRight = itemView.right - deleteIconMargin
+        val deleteIconBottom = deleteIconTop + intrinsicHeight
+
+        // Draw the delete icon
+        deleteIcon?.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom)
+        deleteIcon?.draw(c)
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+    private fun clearCanvas(c: Canvas?, left: Float, top: Float, right: Float, bottom: Float) {
+        c?.drawRect(left, top, right, bottom, clearPaint)
+    }
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        /**
+         * To disable "swipe" for specific item return 0 here.
+         * For example:
+         * if (viewHolder?.itemViewType == YourAdapter.SOME_TYPE) return 0
+         * if (viewHolder?.adapterPosition == 0) return 0
+         */
+        if (viewHolder?.adapterPosition == 10) return 0
+        return super.getMovementFlags(recyclerView, viewHolder)
+    }
+}
