2d8e656188dd69c7278d8537b8a39c32a88e88a5
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-04-01 13:27:31 +1100

    did everything plus the add movie page

--- week5/kit305-android-tutorial-05-main/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
+++ week5/kit305-android-tutorial-05-main/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
@@ -1,6 +1,7 @@
 package au.edu.utas.kit305.tutorial05
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,13 +11,23 @@ import androidx.recyclerview.widget.RecyclerView
 import au.edu.utas.kit305.tutorial05.databinding.ActivityMainBinding
 import au.edu.utas.kit305.tutorial05.databinding.MyListItemBinding
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObject
 import com.google.firebase.ktx.Firebase
 
 val items = mutableListOf<Movie>()
 
+const val FIREBASE_TAG = "FirebaseLogging"
+const val MOVIE_INDEX = "Movie_Index"
+
 class MainActivity : AppCompatActivity()
 {
     private lateinit var ui : ActivityMainBinding
+
+    override fun onResume() {
+        super.onResume()
+
+        ui.myList.adapter?.notifyDataSetChanged() //without a more complicated set-up, we can't be more specific than "dataset changed"
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +44,54 @@ class MainActivity : AppCompatActivity()
         //get db connection
         val db = Firebase.firestore
         Log.d("FIREBASE", "Firebase connected: ${db.app.name}")
+
+        //add some data (comment this out after running the program once and confirming your data is there)
+        val lotr = Movie(
+            title = "Lord of the Rings: Fellowship of the Ring",
+            year = 2001,
+            duration = 9001F
+        )
+
+        val moviesCollectionRef = db.collection("movies")
+
+        /*
+        moviesCollectionRef
+            .add(lotr)
+                .addOnSuccessListener { doc ->
+                    Log.d(FIREBASE_TAG, "Document created with id ${doc.id}")
+                    lotr.id = doc.id
+                }
+                .addOnFailureListener {
+                    Log.e(FIREBASE_TAG, "Error writing document", it)
+                }*/
+
+        ui.lblMovieCount.text = "Loading..."
+        moviesCollectionRef
+            //.whereGreaterThan("year", 2000)
+            .get()
+            .addOnSuccessListener { result ->
+                items.clear() //this line clears the list, and prevents a bug where items would be duplicated upon rotation of screen
+                Log.d(FIREBASE_TAG, "--- all movies ---")
+                for (document in result)
+                {
+                    Log.d(FIREBASE_TAG, document.toString())
+                    val movie = document.toObject<Movie>()
+                    movie.id = document.id
+                    Log.d(FIREBASE_TAG, movie.toString())
+
+                    items.add(movie)
+                }
+
+                ui.lblMovieCount.text = "Movie Count: ${items.size}"
+                (ui.myList.adapter as MovieAdapter).notifyDataSetChanged()
+                //you may choose to fix the warning that notifyDataSetChanged() is not specific enough using:
+                //(ui.myList.adapter as MovieAdapter).notifyItemRangeInserted(0, items.size)
+            }
+
+        ui.btnAdd.setOnClickListener {
+            val i = Intent(this, MovieDetails::class.java)
+            startActivity(i)
+        }
     }
 
     inner class MovieHolder(var ui: MyListItemBinding) : RecyclerView.ViewHolder(ui.root) {}
@@ -50,7 +109,14 @@ class MainActivity : AppCompatActivity()
 
         override fun onBindViewHolder(holder: MainActivity.MovieHolder, position: Int) {
             val movie = movies[position]   //get the data at the requested position
+            holder.ui.txtName.text = movie.title
+            holder.ui.txtYear.text = movie.year.toString()
 
+            holder.ui.root.setOnClickListener {
+                val i = Intent(holder.ui.root.context, MovieDetails::class.java)
+                i.putExtra(MOVIE_INDEX, position)
+                startActivity(i)
+            }
         }
     }
 }
