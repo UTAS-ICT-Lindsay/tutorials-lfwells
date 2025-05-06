6698723dad3cf328c164dc2789cbde379ac32fe5
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-05-06 11:57:35 +1000

    Revert "playing around, will delete"
    This reverts commit 4206a588ef1a07926439ac8dc51b3da98607c2fc.

--- week9/tutorial8/tutorial8/ViewController.swift
+++ week9/tutorial8/tutorial8/ViewController.swift
@@ -7,58 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController,
-                        UITableViewDelegate,
-                        UITableViewDataSource,
-                        UICollectionViewDelegate,
-                        UICollectionViewDataSource,
-                        UIImagePickerControllerDelegate,
-                      UINavigationControllerDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        5
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: "blah", for: indexPath)
-    }
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "test", for: indexPath)
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete
-            {
-                print("Delete!")
-            }
-    }
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.tableView.setEditing(true, animated: true)
-
-        self.navigationItem.rightBarButtonItem = self.editButtonItem
-
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.allowsEditing = true
-        
-        self.present(imagePicker, animated: true, completion: nil)
     }
 
 
